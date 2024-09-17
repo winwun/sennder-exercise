0668af1028037e1c894588353c71cf0128a37c12
winwun <herwin.ochoa@gmail.com>
2024-09-17 11:17:02 +0200

    cleanup

--- src/handlers/sample-handler/__tests__/sample-handler.test.ts
+++ src/handlers/sample-handler/__tests__/sample-handler.test.ts
@@ -2,7 +2,6 @@ import { handler } from '../sample-handler';
 
 describe('Sample hanlder', () => {
   it('should do something', () => {
-    handler();
     expect(handler()).toBe('hello');
   });
 });
